26fa746d6219ed152c38927005b9082d45388b00
axolotl9797 <129421281+axolotl9797@users.noreply.github.com>
2023-08-14 18:05:04 +0100

    Add files via upload

---
+++ Ground.js
@@ -0,0 +1,23 @@
+class Ground {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true,
+    };
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.body = Bodies.rectangle(x, y, width, height, options);
+    World.add(world, this.body);
+  }
+  display(){
+    push();
+    rectMode(CENTER);
+    fill("brown");
+    stroke("black");
+    strokeWeight(4);
+    rect(this.body.position.x,this.body.position.y,this.width,this.height);
+    pop();
+  }
+  
+}
